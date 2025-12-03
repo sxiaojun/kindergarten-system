7e613b63e47a95e81cbcae7c948e5ba6cd45ed2a
songxiaojun <songxiaojun@dianxiaomi.com>
2025-12-03 19:27:21 +0800

    改mysql配置

--- kindergarten_system/kindergarten_system/settings.py
+++ kindergarten_system/kindergarten_system/settings.py
@@ -86,10 +86,24 @@ WSGI_APPLICATION = 'kindergarten_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kindergarten_db',           # 数据库名称
+        'USER': '',       # MySQL用户名
+        'PASSWORD': '',   # MySQL密码
+        'HOST': 'localhost',                 # 数据库主机
+        'PORT': '3306',                      # 数据库端口
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
